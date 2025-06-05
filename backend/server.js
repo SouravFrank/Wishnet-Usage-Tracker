134b32db142f19e7d0e877d336cb54cbae90678f
Sourav Sadhukhan <ssadhukhan990@gmail.com>
2025-06-05 20:40:21 +0530

    data update

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const HEADERS = {
   "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
   "accept-language": "en-IN,en-US;q=0.9,en-GB;q=0.8,en;q=0.7",
   "upgrade-insecure-requests": "1",
-  "cookie": "JSESSIONID=78407236A605800351936E0D6E35F299",
+  "cookie": "JSESSIONID=D1EEC521C78F7B3EDDC0FB8299AF3342",
   "Referrer-Policy": "strict-origin-when-cross-origin"
 };
 
